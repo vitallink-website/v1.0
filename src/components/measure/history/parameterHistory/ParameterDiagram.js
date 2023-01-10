669f9bb133766d2259eca4e45724a6794375c72d
Parna Asadi <parna80as@gmail.com>
2023-01-10 11:39:50 +0330

    add Parameter

---
+++ src/components/measure/history/parameterHistory/ParameterDiagram.js
@@ -0,0 +1,35 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import Diagram from "../../../Diagram/Diagram";
+import { useEffect, useState } from "react";
+
+const ParameterDiagram = () => {
+  const { type } = useParams();
+  const [flow, setFlow] = useState([]);
+
+  useEffect(() => {
+    // type is the name of database
+    // get table in here
+    // map all data to  { id: "time" , value: caculated value(for example ECGheartbeat)}
+    // eg: const temp = data.map(row => ({id: new Date(row.date),value:row.heartbeat}))
+    // then set it in the flow : setFlow(temp)
+    setFlow([]);
+  }, []);
+
+  return (
+    <Container className="history-section">
+      <Row>
+        <Diagram dataKey={type} flow={flow} />
+      </Row>
+      <Row>
+        <Col>
+          <Link to="/Measure/History/ParameterHistory">
+            <Button>Up</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ParameterDiagram;
